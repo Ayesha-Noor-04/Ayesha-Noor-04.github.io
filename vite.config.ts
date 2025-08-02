bea4b8a4d16fef8b5523068b492effa4b04485df
Ayesha Noor <ayeshanr0@gmail.com>
2025-08-02 22:11:05 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/AyeshaNoor/", // ✅ MUST BE PRESENT
+  base: "/", // ✅ Because the repo is now your root site
   plugins: [react()],
   resolve: {
     alias: {
